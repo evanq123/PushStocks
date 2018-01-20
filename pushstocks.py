4cadd1176a1ec5bff525cfbe88ef3fc32ec6d62f
evanq123 <evanq123@gmail.com>
2018-01-20 01:40:46 -0500

    starting work on version 2

--- pushstocks.py
+++ pushstocks.py
@@ -1,9 +1,8 @@
 import sys
 sys.path.insert(0, "lib")
-import requests
-import re
 import time
-import datetime
+from datetime import datetime
+from urllib.request import urlopen
 
 try:
     from bs4 import BeautifulSoup
@@ -13,30 +12,45 @@ except ImportError:
           "https://github.com/evanq123/PushStocks \n")
     sys.exit(1)
 
+api_key         = input("Enter your PushBullet api key: ")
+if use_crypto:
+    currency    = input("Enter the currency(full name): ").upper()
+    threshold   = float(input("Enter the amount in BTC to start notifying: "))
+else:
+    symbol      = input("Enter the symbol: ").upper()
+    threshold   = float(input("Enter the amount in USD to start notifying: "))
 
-api_key     = input("Enter your PushBullet api key: ")
-symbol      = input("Enter the symbol: ").upper()
-threshold   = float(input("Enter the amount in USD to start notifying: "))
-intervals   = int(input("Enter the time (in seconds) between checks: "))
+intervals       = int(input("Enter the time intervals (s) between checks: "))
+
+def get_rate(currency): #This works, but is very slow
+    currency = currency.replace(" ", "-")
+    base_url = 'http://coinmarketcap.com/currencies/'
+    url = urlopen(base_url + currency + "/")
+    soup = BeautifulSoup(url, "html.parser")
+    rate = soup.find('span', attrs={"class": "text-gray details-text-medium"}).text
+    return rate
 
 
 def get_quote(symbol):
     base_url = 'http://finance.google.com/finance?q='
-    html = requests.get(base_url + symbol).text
-    soup = BeautifulSoup(html, "html.parser")
+    url = urlopen(base_url + symbol)
+    soup = BeautifulSoup(url, "html.parser")
     quote = soup.find('span', attrs={'class': 'pr'}).text
     return quote
 
 
-def push_message(msg, symbol):
+def push_message(msg):
     pb = PushBullet(api_key)
-    pb.push_note("PushStocks: The quote for {} has changed!".format(symbol), msg)
+    if use_crypto:
+        pb.push_note("PushStocks: The rate for {} has changed!".format(currency), msg)
+    else:
+        pb.push_note("PushStocks: The quote for {} has changed!".format(symbol), msg)
 
 
 message_sent = False # Hackish workaround for now.
 def price_past_threshold():
     non_decimal = re.compile(r'[^\d.]+')
-    quote = non_decimal.sub('', get_quote(symbol))
+    quote = non_decimal.sub('', get_quote(currency))
     if float(quote) >= float(threshold):
         return True
 
@@ -44,17 +58,29 @@ def price_past_threshold():
 while True:
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     if price_past_threshold() and message_sent is False:
-        msg = ("As of, {} EST, the quote for {} is >= {} USD (at {} USD)"
-               "".format(date, symbol, threshold, get_quote(symbol).strip('\n')))
-        print("Sending message to PushBullet...\n")
-        push_message(msg, symbol)
+        if use_crypto:
+            msg = ("As of, {} EST, the rate for {} is >= {} BTC (at {} BTC)"
+                   "".format(date, currency, threshold, get_quote(currency)))
+            print("Sending message to PushBullet...\n")
+            push_message(msg, currency)
+        else:
+            msg = ("As of, {} EST, the quote for {} is >= {} USD (at {} USD)"
+                   "".format(date, symbol, threshold, get_quote(symbol).strip('\n')))
+            print("Sending message to PushBullet...\n")
+            push_message(msg, symbol)
         message_sent = True
 
     if not price_past_threshold() and message_sent is True:
-        msg = ("As of, {} EST, the quote for {} is < {} USD (at {} USD)"
-               "".format(date, symbol, threshold, get_quote(symbol)))
-        print("Sending message to PushBullet...\n")
-        push_message(msg, symbol)
+        if use_crypto:
+            msg = ("As of, {} EST, the rate for {} is < {} BTC (at {} BTC)"
+                   "".format(date, currency, threshold, get_quote(currency)))
+            print("Sending message to PushBullet...\n")
+            push_message(msg, currency)
+        else:
+            msg = ("As of, {} EST, the quote for {} is < {} USD (at {} USD)"
+                   "".format(date, symbol, threshold, get_quote(symbol)))
+            print("Sending message to PushBullet...\n")
+            push_message(msg, symbol)
         message_sent = False
 
     time.sleep(intervals)
