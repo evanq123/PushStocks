85fa55fba51427d26a0e4301ffa04000eb6e732b
evanq123 <evanq123@gmail.com>
2018-01-19 20:35:53 -0500

    [v1.0] Version 1 release

--- pushstocks.py
+++ pushstocks.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.insert(0, "lib")
 import requests
+import re
+import time
+from datetime import datetime
 
 try:
     from bs4 import BeautifulSoup
@@ -11,6 +14,12 @@ except ImportError:
     sys.exit(1)
 
 
+api_key     = input("Enter your PushBullet api key: ")
+symbol      = input("Enter the symbol: ")
+threshold   = input("Enter the amount in USD to start notifying: ")
+intervals   = input("Enter the time intervals (s) between checks: ")
+
+
 def get_quote(symbol):
     base_url = 'http://finance.google.com/finance?q='
     html = requests.get(base_url + symbol).text
@@ -19,23 +28,21 @@ def get_quote(symbol):
     return quote
 
 
-def push_message(msg):
-    try:
-        pb = PushBullet(api_key)
-        pb.push_note("PushStocks: Quotes have changed!", msg)
-    except InvalidKeyError:
-        print("An invalid API key was entered. Please consult guide at\n"
-              "https://github.com/evanq123/PushStocks \n")
+def push_message(msg, symbol):
+    pb.push_note("PushStocks: The quote for {} have changed!".format(symbol), msg)
 
-send_message = False
-for symbol in symbols:
-    quote = get_quote(symbol)
-    msg = ''
+
+def send_message():
+    non_decimal = re.compile(r'[^\d.]+')
+    quote = non_decimal.sub('', get_quote(symbol))
     if float(quote) >= float(threshold):
-        msg = msg + "Price for {} went up to {} "
-                    "(threshold = {})\n".format(symbol, quote, threshold)
-        send_message = True
+        return True
 
-    if send_message:
-        print("Sending message to PushBullet...\n")
-        push_message(msg)
+
+while True:
+    if send_message():
+        msg = ("Price for {} went up to {} (threshold = {})\n"
+               "".format(symbol, get_quote(symbol), threshold))
+        print("Sending messgae to PushBullet...\n")
+        push_message(msg, symbol)
+    time.sleep(intervals)
